cdef79605958225b4a8106793be24eb939eb8dcc
mahdiBazar <mbadienew@gmail.com>
2025-02-26 15:51:26 +0330

    Added TestComment

---
+++ Tokenization1.py
@@ -0,0 +1,15 @@
+from transformers import AutoTokenizer
+
+text = "کتابخانه‌های عمومی منابع ارزشمندی برای یادگیری هستند."
+
+# مدل‌های مختلف توکن‌سازی
+tokenizers = {
+    "BERT" : AutoTokenizer.from_pretrained("bert-base-multilingual-cased"),
+    "GPT-2": AutoTokenizer.from_pretrained("gpt2")
+}
+
+# پردازش متن با توکن‌سازها
+for model, tokenizer in tokenizers.items():
+    tokens = tokenizer.tokenize(text)
+    print(f"\n{model} Tokenization:")
+    print(tokens)
