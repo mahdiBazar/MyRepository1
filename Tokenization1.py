f41aedc54943a4c80a48122febc3d0e833273a3d
mahdiBazar <mbadienew@gmail.com>
2025-02-26 16:00:06 +0330

    Added Change1

--- Tokenization1.py
+++ Tokenization1.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer
 
 text = "کتابخانه‌های عمومی منابع ارزشمندی برای یادگیری هستند."
 
-# مدل‌های مختلف توکن‌سازی
+# مدل‌های مختلف توکن‌سازی chang1
 tokenizers = {
     "BERT" : AutoTokenizer.from_pretrained("bert-base-multilingual-cased"),
     "GPT-2": AutoTokenizer.from_pretrained("gpt2")
